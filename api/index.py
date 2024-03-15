5ed4a362d5afd42b26291f2620a0146d6c975397
Ariasaka <66897357+LYXOfficial@users.noreply.github.com>
2024-03-15 14:23:11 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -34,12 +34,12 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
         user = path.split('?')[1]
-        self.wfile.write(user.encode('utf-8'))
-        return
-        data = getdata(user)
+        #data = getdata(user)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
+        self.wfile.write(user.encode('utf-8'))
+        return
         self.wfile.write(json.dumps(data).encode('utf-8'))
         return
